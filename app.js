1f8cb17cfead236f93598ef449a2904d08d6a596
OpenTech-Consult <opentech.consult@gmail.com>
2021-12-30 16:41:55 +0000

    Node Basics Assignment
    Node Basics Assignment first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// Spin up a Node.js server (on port 3000)
+// Handle two routes: "/" and "/users"
+// - Return some greeting text on  "/"
+// - Return a list of dummy users on "/users" (e.g. <ul><li>user 1</li></ul>)
+// Add a form with a "username" <input> to the "/" page and submit a POST request 
+// to "/create-user" upon  a button click.
+// Add the "/create-user" route and parse 
