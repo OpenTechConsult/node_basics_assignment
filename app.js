c50db264786cf676729c62103772b1a808535829
OpenTech-Consult <opentech.consult@gmail.com>
2021-12-31 17:19:13 +0000

    Spun up a Node.js web server on port 3000 Handled two routes "/" and "/users" Returned some greeting text on "/" Returned a list of dommy users on "/users" Added a form with a username input vto the "/" page and submit the POST request to "/create-user" upon a button click Added the "/create-route" and parse the incoming form data Logged the inputed data to the console.

--- app.js
+++ app.js
@@ -1,7 +1,55 @@
-// Spin up a Node.js server (on port 3000)
-// Handle two routes: "/" and "/users"
-// - Return some greeting text on  "/"
-// - Return a list of dummy users on "/users" (e.g. <ul><li>user 1</li></ul>)
-// Add a form with a "username" <input> to the "/" page and submit a POST request 
-// to "/create-user" upon  a button click.
-// Add the "/create-user" route and parse 
+const http = require('http');
+
+http.createServer(function (request, response) {
+    const url = request.url;
+    response.setHeader('Content-Type', 'text/html');
+    
+    if (url === '/') {
+        response.write('<html>');
+        response.write('<head><title>Enter a User</title></head>');
+        response.write('<body>');
+        response.write('<h1>Hi User. </h1>');
+        response.write('<form method="POST" action="/create-user">');
+        response.write('<label for="username">Enter your username </label>');
+        response.write('<input type="text" name="username" placeholder="username">');
+        response.write('<button type="submit">Submit</button>');
+        response.write('</form>');
+        response.write('</body>');
+        return response.end();
+    }
+
+    if (url === '/users') {
+        response.write('<html>');
+        response.write('<head><title>Users</title></head>');
+        response.write('<body>');
+        response.write('<h1>Users</h1>');
+        response.write('<ul>');
+        response.write('<li>User 1</li>');
+        response.write('<li>User 2</li>');
+        response.write('<li>User 3</li>');
+        response.write('</ul>');
+        response.write('</body>');
+        return response.end();
+    }
+
+    if (url === '/create-user' && request.method === 'POST') {
+        const body = [];
+
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        return request.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody.split('=')[1]);
+            response.statusCode = 302;
+            response.setHeader('Location', '/');
+            response.end();
+        });
+
+    }
+    response.end();
+
+}).listen(3000);
+
+console.log('Server running at http://127.0.0.1:3000/');
